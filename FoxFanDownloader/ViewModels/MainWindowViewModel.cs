780c1505c938f1112b70dcd4b5043e2611297cb1
Дмитрий Смирнов <painven@gmail.com>
2022-04-08 17:26:59 +0300

    Добавлен проброс UI визуального конструктора

--- FoxFanDownloader/ViewModels/MainWindowViewModel.cs
+++ FoxFanDownloader/ViewModels/MainWindowViewModel.cs
@@ -38,29 +38,48 @@ public class MainWindowViewModel : ViewModelBase
         get => inProgress;
         set => Set(ref inProgress, value);
     }
-    public MainWindowViewModel(ISettingsStorage settingsStorage, FoxFanParser parser)
-    {     
-        ParseMultfimCommand = new LambdaCommand(ParseMultfilm, e => false);
-        LoadedCommand = new LambdaCommand(Loaded, e => !InProgress);
-        Multfilms = new ObservableCollection<Multfilm>();      
+    public MainWindowViewModel(ISettingsStorage settingsStorage, FoxFanParser parser) : this()
+    {      
         this.settingsStorage = settingsStorage;
         this.parser = parser;
+    }
+    public MainWindowViewModel()
+    {
+        ParseMultfimCommand = new LambdaCommand(ParseMultfilm, e => false);
+        LoadedCommand = new LambdaCommand(Loaded, e => !InProgress);
+        Multfilms = new ObservableCollection<Multfilm>();
+        LoadParsedData();
+    }
+    private void LoadParsedData()
+    {
+        string storageDir = Path.GetDirectoryName(Application.Current.GetType().Assembly.Location) + "\\storage";
+        foreach (var jsonFile in Directory.GetFiles(storageDir, "*.json"))
+        {
+            Multfilms.Add(JsonConvert.DeserializeObject<Multfilm>(File.ReadAllText(jsonFile)));
+        }
+        if (settings == null)
+        {
+            if(Multfilms.Any())
+            {
+                SelectedMultfilm = Multfilms.First();
+            }
+            if (SelectedMultfilm.SeasonsInfo.Seasons.Any())
+            {
+                SelectedMultfilm.SeasonsInfo.SelectedSeason = SelectedMultfilm.SeasonsInfo.Seasons.FirstOrDefault();
+            }
+        }
     }
     private void Loaded(object obj)
     {
         Multfilms.Clear();
-        string storageDir = Path.GetDirectoryName(Application.Current.GetType().Assembly.Location) + "\\storage";
-        foreach (var jsonFile in Directory.GetFiles(storageDir, "*.json")) 
-        {
-            Multfilms.Add(JsonConvert.DeserializeObject<Multfilm>(File.ReadAllText(jsonFile)));
-        }
 
+        LoadParsedData();
         settings = settingsStorage.GetSettings();
         SelectedMultfilm = Multfilms.FirstOrDefault(m => m.Name == settings.SelectedMultfilmName);
         IsOneLineSubtitles = settings.IsOneLineSubtitles;
-        if (selectedMultfilm?.SeasonsInfo != null)
+        if (SelectedMultfilm?.SeasonsInfo != null)
         {
-            selectedMultfilm.SeasonsInfo.SelectedSeason = selectedMultfilm.SeasonsInfo.Seasons?.FirstOrDefault(s => s.Number == settings.SelectedSeasonNumber);
+            SelectedMultfilm.SeasonsInfo.SelectedSeason = selectedMultfilm.SeasonsInfo.Seasons?.FirstOrDefault(s => s.Number == settings.SelectedSeasonNumber);
         }
 
         foreach(var m in Multfilms)
